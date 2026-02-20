e9f56100a4d013662b99e8cf4e2084f3cfaf67e1
fabianapereira23-ui <fabianapereira23@outlook.pt>
2026-02-20 15:50:38 +0000

    Adiciona função para listar reservas

--- reservas.py
+++ reservas.py
@@ -3,3 +3,7 @@ reservas = []
 def criar_reserva(nome_cliente):
     reservas.append(nome_cliente)
     print("Reserva criada.")
+
+def listar_reservas():
+    for reserva in reservas:
+        print(reserva)
