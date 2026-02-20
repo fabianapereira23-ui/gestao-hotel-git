07a8b2983fd1b364b9082f822fa9d1da9ef46096
fabianapereira23-ui <fabianapereira23@outlook.pt>
2026-02-20 15:06:09 +0000

    Adiciona função para remover cliente

--- clientes.py
+++ clientes.py
@@ -8,3 +8,11 @@ def listar_clientes():
     print("Lista de clientes:")
     for cliente in clientes:
         print(cliente)
+
+
+def remover_cliente(nome_cliente):
+    if nome_cliente in clientes:
+        clientes.remove(nome_cliente)
+        print("Cliente removido com sucesso.")
+    else:
+        print("Cliente não encontrado.")
