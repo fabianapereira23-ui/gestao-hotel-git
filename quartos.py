8a680fb974300f16d36c44744600600bfd480732
fabianapereira23-ui <fabianapereira23@outlook.pt>
2026-02-20 15:41:24 +0000

    Adiciona função para mostrar estado do quarto

--- quartos.py
+++ quartos.py
@@ -2,7 +2,10 @@ quartos = {}
 def adicionar_quarto(numero):
     quartos[numero] = "disponivel"
     print("Quarto adicionado como disponível.")
-    
+
 def listar_quartos():
     for numero in quartos:
         print(numero)
+
+def estado_quarto(numero):
+    print(quartos[numero])
